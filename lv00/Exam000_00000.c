0451e5c059fe8a4fc0a532a2a77d0f67692ddb24
fp024 <aopenasus@naver.com>
2025-06-04 14:18:35 +0900

    GoogleTest 추가
    좀 더 단순화하거나 수정을 해야겠지만..😅
    테스트가 정상 동작하는 수준까지 되었으니
    일단 커밋...

--- lv00/Exam000_00000.c
+++ lv00/Exam000_00000.c
@@ -1,6 +1,6 @@
 #include "CommonUtils.h"
 
-void BubbleSort(int arr[], const int length) {
+void solution(int arr[], const int length) {
   for (int i = 0; i < length - 1; i++) {
     // 아래 반복문 조건 잘못 설정했었음.
     for (int j = 0; j < length - i - 1; j++) {
@@ -10,13 +10,6 @@ void BubbleSort(int arr[], const int length) {
       printArraySimple(arr, length);
     }
   }
-}
-
-int main() {
-  int numbers[] = {3, 2, 4, 1};
-  const int length = sizeof(numbers) / sizeof(int);
-  BubbleSort(numbers, length);
-  return 0;
 }
 
 /*
---
+++ lv00/Exam000_00000_test.cpp
@@ -0,0 +1,107 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+extern "C" {
+void solution(int arr[], int length);
+}
+
+using testing::ElementsAreArray;
+
+class Exam000_00000_Tests : public testing::Test {
+protected:
+    void SetUp() override {
+        // 각 테스트 전에 실행
+    }
+
+    void TearDown() override {
+        // 각 테스트 후에 실행
+    }
+};
+
+// 기본 정렬 테스트
+TEST_F(Exam000_00000_Tests, BasicSorting) {
+    int arr[] = {3, 2, 4, 1};
+    constexpr int expected[] = {1, 2, 3, 4};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 이미 정렬된 배열 테스트
+TEST_F(Exam000_00000_Tests, AlreadySorted) {
+    int arr[] = {1, 2, 3, 4, 5};
+    constexpr int expected[] = {1, 2, 3, 4, 5};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 역순 정렬된 배열 테스트
+TEST_F(Exam000_00000_Tests, ReverseSorted) {
+    int arr[] = {5, 4, 3, 2, 1};
+    constexpr int expected[] = {1, 2, 3, 4, 5};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 중복 값이 있는 배열 테스트
+TEST_F(Exam000_00000_Tests, DuplicateValues) {
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5};
+    constexpr int expected[] = {1, 1, 2, 3, 4, 5, 5, 6, 9};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 단일 원소 배열 테스트
+TEST_F(Exam000_00000_Tests, SingleElement) {
+    int arr[] = {42};
+    constexpr int expected[] = {42};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 두 원소 배열 테스트
+TEST_F(Exam000_00000_Tests, TwoElements) {
+    int arr[] = {2, 1};
+    constexpr int expected[] = {1, 2};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 음수가 포함된 배열 테스트
+TEST_F(Exam000_00000_Tests, NegativeNumbers) {
+    int arr[] = {-3, 1, -2, 5, 0};
+    constexpr int expected[] = {-3, -2, 0, 1, 5};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
+
+// 큰 배열 테스트
+TEST_F(Exam000_00000_Tests, LargeArray) {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90, 88, 76, 50, 42};
+    constexpr int expected[] = {11, 12, 22, 25, 34, 42, 50, 64, 76, 88, 90};
+    constexpr int length = std::size(arr);
+
+    solution(arr, length);
+
+    EXPECT_THAT(arr, ElementsAreArray(expected, length));
+}
