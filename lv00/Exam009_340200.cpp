b3c98f1f6e1be90f17f6af1049d10dd223110a87
fp024 <aopenasus@naver.com>
2025-06-06 07:23:04 +0900

    [PCCE 기출문제] 8번 / 닉네임 규칙 - 340200

---
+++ lv00/Exam009_340200.cpp
@@ -0,0 +1,39 @@
+#include <string>
+
+using namespace std;
+
+/**
+ * [PCCE 기출문제] 8번 / 닉네임 규칙
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340200
+ */
+string solution(const string &nickname) {
+    string answer;
+    for (const char i: nickname) {
+        if (i == 'l') {
+            answer += "I";
+        } else if (i == 'w') {
+            answer += "vv";
+        } else if (i == 'W') {
+            answer += "VV";
+        } else if (i == 'O') {
+            answer += "0";
+        } else {
+            answer += i;
+        }
+    }
+    while (answer.size() < 4) {
+        answer += "o";
+    }
+    if (answer.size() > 8) {
+        answer = answer.substr(0, 8);
+    }
+    return answer;
+}
+
+//
+// === 문제 읽고 첫 느낌 ===
+// ...
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
---
+++ lv00/Exam009_340200_test.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+using namespace std;
+using namespace testing;
+
+// 테스트할 함수 선언
+string solution(const string &nickname);
+
+class Exam009_340200_Tests : public Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+// 기본 테스트 케이스들
+TEST_F(Exam009_340200_Tests, DefaultTestCase01) {
+    const string result = solution("WORLDworld");
+    EXPECT_EQ(result, "VV0RLDvv");
+}
+
+TEST_F(Exam009_340200_Tests, DefaultTestCase02) {
+    const string result = solution("GO");
+    EXPECT_EQ(result, "G0oo");
+}
