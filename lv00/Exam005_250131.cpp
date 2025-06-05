bce40cf297d0c74fc1f47b8e18c6c579a3c69e37
fp024 <aopenasus@naver.com>
2025-06-06 05:34:04 +0900

    [PCCE 기출문제] 3번 / 나이 계산 - 250131
    이번에는 solution함수의 cin을 그대로 두고
    테스트를 해보았다.
    
    원래의 stdin을 잠깐 보관하고 까짜 stdin입력으로
    입력해주니 잘되었음.
    
    CommonUtils 사용 관련해서 CMake 설정이
    이상한 부분을 단순화 하였다.

---
+++ lv00/Exam005_250131.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * [PCCE 기출문제] 3번 / 나이 계산
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250131
+ */
+int solution() {
+    int year, answer = 0;
+    string age_type;
+    cin >> year >> age_type;
+
+    if (age_type == "Korea") {
+        answer = 2030 - year + 1;
+    } else if (age_type == "Year") {
+        answer = 2030 - year;
+    }
+
+    cout << answer << endl;
+    return 0;
+}
+
+//
+// === 문제 읽고 첫 느낌 ===
+//   cin, cout에 대해서 완전하게 테스트를 해볼까?
+//   역시 방법이 있었다. 예전에 Java+JUnit 환경에서 진행했던 내용과 비슷하게 되었음.
+//   * https://github.com/fp024/my-utils/blob/master/my-test-util/src/main/java/org/fp024/test/util/keyboard/KeyboardInputTestHelper.java
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
---
+++ lv00/Exam005_250131_test.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <sstream>
+#include <iostream>
+
+
+using namespace std;
+using namespace testing;
+
+// 테스트할 함수 선언
+int solution();
+
+class Exam005_250131_Tests : public Test {
+protected:
+    std::streambuf *orig_cin = nullptr;
+
+    void SetUp() override {
+        orig_cin = std::cin.rdbuf(); // 원래 stdin 저장
+    }
+
+    void TearDown() override {
+        std::cin.rdbuf(orig_cin); // 원래 stdin 복원
+    }
+
+    static void setInput(const string &input) {
+        static std::istringstream input_stream;
+        input_stream.str(input);
+        input_stream.clear();
+        std::cin.rdbuf(input_stream.rdbuf()); // 가짜 stdin으로 대체
+    }
+};
+
+// 기본 테스트 케이스들
+TEST_F(Exam005_250131_Tests, DefaultTestCase01) {
+    setInput("2000\nKorea\n"); // 예시 입력
+
+    internal::CaptureStdout();
+    solution();
+    const string output = internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "31\n");
+}
+
+TEST_F(Exam005_250131_Tests, DefaultTestCase02) {
+    setInput("1999 Year"); // 💡 공백으로 구분해서 설정해도 된다.
+
+    internal::CaptureStdout();
+    solution();
+    const string output = internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "31\n");
+}
