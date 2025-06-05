6c0299db1de2106546a9f684d1db18159b7f51ff
fp024 <aopenasus@naver.com>
2025-06-06 06:10:35 +0900

    [PCCE 기출문제] 7번 / 가습기 - 250127
    뭔가 함정을 만드는 것 같다.
    그 함정을 구분하려고 ExtraTestCase01 추가함 😅

---
+++ lv00/Exam006_250127.cpp
@@ -0,0 +1,53 @@
+#include <string>
+
+using namespace std;
+
+/**
+ * [PCCE 기출문제] 7번 / 가습기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250127
+ */
+int func1(const int humidity, const int val_set) {
+    if (humidity < val_set)
+        return 3;
+    return 1;
+}
+
+int func2(const int humidity) {
+    if (humidity >= 50)
+        return 0;
+    if (humidity >= 40)
+        return 1;
+    if (humidity >= 30)
+        return 2;
+    if (humidity >= 20)
+        return 3;
+    if (humidity >= 10)
+        return 4;
+    return 5;
+}
+
+int func3(int humidity, int val_set) {
+    if (humidity < val_set)
+        return 1;
+    return 0;
+}
+
+int solution(const string &mode_type, const int humidity, const int val_set) {
+    int answer = 0;
+    if (mode_type == "auto") {
+        answer = func2(humidity);
+    } else if (mode_type == "target") {
+        answer = func1(humidity, val_set);
+    } else if (mode_type == "minimum") {
+        answer = func3(humidity, val_set);
+    }
+    return answer;
+}
+
+//
+// === 문제 읽고 첫 느낌 ===
+// ...
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
---
+++ lv00/Exam006_250127_test.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <sstream>
+#include <iostream>
+
+
+using namespace std;
+using namespace testing;
+
+// 테스트할 함수 선언
+int solution(const string &mode_type, int humidity, int val_set);
+
+class Exam006_250127_Tests : public Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+// 기본 테스트 케이스들
+TEST_F(Exam006_250127_Tests, DefaultTestCase01) {
+    const int result = solution("auto", 23, 45);
+    EXPECT_EQ(result, 3);
+}
+
+TEST_F(Exam006_250127_Tests, DefaultTestCase02) {
+    const int result = solution("target", 41, 40);
+    EXPECT_EQ(result, 1);
+}
+
+TEST_F(Exam006_250127_Tests, DefaultTestCase03) {
+    const int result = solution("minimum", 10, 34);
+    EXPECT_EQ(result, 1);
+}
+
+TEST_F(Exam006_250127_Tests, ExtraTestCase01) {
+    const int result = solution("minimum", 34, 10);
+    EXPECT_EQ(result, 0);
+}
