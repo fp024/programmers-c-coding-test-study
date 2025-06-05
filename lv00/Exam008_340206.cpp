ca0b44f235c838c4bdd502d6c25784d9eba7a81c
fp024 <aopenasus@naver.com>
2025-06-06 07:09:40 +0900

    [PCCE 기출문제] 2번 / 각도 합치기 - 340206
    solution 함수의 cin, cout를 그대로 유지했다. 😅

---
+++ lv00/Exam008_340206.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * [PCCE 기출문제] 2번 / 각도 합치기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340206
+ */
+int solution() {
+    int angle1;
+    int angle2;
+    cin >> angle1 >> angle2;
+
+    const int sum_angle = (angle1 + angle2) % 360;
+    cout << sum_angle << endl;
+    return 0;
+}
+
+//
+// === 문제 읽고 첫 느낌 ===
+// ...
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
---
+++ lv00/Exam008_340206_test.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <sstream>
+#include <iostream>
+
+
+using namespace std;
+using namespace testing;
+
+// 테스트할 함수 선언
+int solution();
+
+class Exam008_340206_Tests : public Test {
+protected:
+    std::streambuf *orig_cin = nullptr;
+
+    void SetUp() override {
+        orig_cin = std::cin.rdbuf(); // 원래 stdin 저장
+    }
+
+    void TearDown() override {
+        std::cin.rdbuf(orig_cin); // 원래 stdin 복원
+    }
+
+    static void setInput(const string &input) {
+        static std::istringstream input_stream;
+        input_stream.str(input);
+        input_stream.clear();
+        std::cin.rdbuf(input_stream.rdbuf()); // 가짜 stdin으로 대체
+    }
+};
+
+// 기본 테스트 케이스들
+TEST_F(Exam008_340206_Tests, DefaultTestCase01) {
+    setInput("280\n485\n"); // 예시 입력
+
+    internal::CaptureStdout();
+    solution();
+    const string output = internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "45\n");
+}
