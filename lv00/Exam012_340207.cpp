2e48fe3660f7108d85bd4c396fd7395b814708fd
fp024 <aopenasus@naver.com>
2025-06-06 08:03:40 +0900

    [PCCE 기출문제] 1번 / 문자 출력 - 340207

---
+++ lv00/Exam012_340207.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * [PCCE 기출문제] 1번 / 문자 출력
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340207
+ */
+int solution() {
+    const string message = "Let's go!";
+
+    cout << "3\n2\n1" << endl;
+    cout << message << endl;
+
+    return 0;
+}
+
+//
+// === 문제 읽고 첫 느낌 ===
+// ...
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
---
+++ lv00/Exam012_340207_test.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include <iostream>
+
+
+using namespace std;
+using namespace testing;
+
+// 테스트할 함수 선언
+int solution();
+
+class Exam012_340207_Tests : public Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+// 기본 테스트 케이스들
+TEST_F(Exam012_340207_Tests, DefaultTestCase01) {
+    internal::CaptureStdout();
+    solution();
+    const string output = internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "3\n2\n1\nLet's go!\n");
+}
