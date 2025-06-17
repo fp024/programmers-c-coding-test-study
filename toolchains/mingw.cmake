e279a762c01258afae3339ce412519888231ef34
fp024 <aopenasus@naver.com>
2025-06-17 16:46:36 +0900

    mingw 설정 조정

--- toolchains/mingw.cmake
+++ toolchains/mingw.cmake
@@ -6,6 +6,3 @@ message(STATUS "Configuring for preset: mingw")
 set(CMAKE_C_COMPILER "${MINGW_ROOT}/bin/gcc.exe" CACHE FILEPATH "C compiler for MinGW")
 set(CMAKE_CXX_COMPILER "${MINGW_ROOT}/bin/g++.exe" CACHE FILEPATH "CXX compiler for MinGW")
 set(CMAKE_MAKE_PROGRAM "${NINJA_ROOT}/ninja.exe" CACHE FILEPATH "Ninja executable")
-
-# MinGW의 경우 bin 폴더를 PATH에 추가
-set(ENV{PATH} "${MINGW_ROOT}/bin;$ENV{PATH}")
