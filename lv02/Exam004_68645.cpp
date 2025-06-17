7373c28129fe4d3a6b1f8e10f1739f47134eef64
fp024 <aopenasus@naver.com>
2025-06-17 09:52:24 +0900

    삼각 달팽이 - 68645

---
+++ lv02/Exam004_68645.cpp
@@ -0,0 +1,82 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/**
+ * 삼각 달팽이 - ✨✨
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/68645
+ */
+
+vector<int> solution(int n) {
+  // 방향 지정: 아래, 오른쪽, 왼쪽위
+  const int dx[] = {0, 1, -1};
+  const int dy[] = {1, 0, -1};
+
+  // 삼각형 데이터를 넣을 벡터
+  vector<vector<int>> triangle(n, vector<int>(n));
+
+  int x = 0;
+  int y = 0;
+  int d = 0;
+  int v = 1;
+
+  while (true) {
+    triangle[y][x] = v++;
+
+    int y_next = y + dy[d];
+    int x_next = x + dx[d];
+    // 다음 이동이 유효하지 않으면 방향 변경
+    if (y_next >= n ||                 //
+        y_next < 0 ||                  //
+        x_next >= n ||                 //
+        x_next < 0 ||                  //
+        triangle[y_next][x_next] != 0  //
+    ) {
+      // 방향 변경
+      d = (d + 1) % 3;
+      //
+      // 방향 변경 직후 다음 변경이 유효하지 않으면 종료
+      y_next = y + dy[d];
+      x_next = x + dx[d];
+      if (y_next >= n ||                 //
+          y_next < 0 ||                  //
+          x_next >= n ||                 //
+          x_next < 0 ||                  //
+          triangle[y_next][x_next] != 0  //
+      ) {
+        break;
+      }
+    }
+
+    // 이동
+    y = y + dy[d];
+    x = x + dx[d];
+  }
+
+  // 답변 배열 채워넣기
+  vector<int> answer;
+
+  int index = 0;
+  for (int i = 0; i < triangle.size(); i++) {
+    for (int j = 0; j <= i; j++) {
+      answer.push_back(triangle[i][j]);
+    }
+  }
+
+  return answer;
+}
+
+//
+// === 문제 읽고 첫 느낌 ===
+//   이 내용은...
+//   "프로그래머스 코딩 테스트 문제 풀이 전략: 자바편"의 내용을 한번 공부해보고,
+//   C++로 풀어본 내용이다. 저자님께서 문제를 이해하기 쉽게 풀어내신다. 👍👍
+//   그리고...
+//   VSCode + CMake Tools 환경에서 진행해봤는데, 디버깅하고 중단점 찍고 값
+//   확인하고 하는 것들이 괜찮았다. +1점으로 통과 👍
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+//
---
+++ lv02/Exam004_68645_test.cpp
@@ -0,0 +1,66 @@
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace testing;
+
+/** === 💡 테스트할 함수 선언 💡 === */
+// 문제 해결 함수
+vector<int> solution(int n);
+
+// 기본 테스트 케이스들
+TEST(Exam004_68645_Tests, Default_TestCase_01) {
+  const vector<int> result = solution(4);
+  EXPECT_THAT(result, ContainerEq(  //
+                          vector<int>{
+                              1,          //
+                              2, 9,       //
+                              3, 10, 8,   //
+                              4, 5, 6, 7  //
+                          }  //
+                          )  //
+  );
+}
+
+TEST(Exam004_68645_Tests, Default_TestCase_02) {
+  const vector<int> result = solution(5);
+  EXPECT_THAT(result, ContainerEq(  //
+                          vector<int>{
+                              1,              //
+                              2, 12,          //
+                              3, 13, 11,      //
+                              4, 14, 15, 10,  //
+                              5, 6, 7, 8, 9   //
+                          }  //
+                          )  //
+  );
+}
+
+TEST(Exam004_68645_Tests, Default_TestCase_03) {
+  const vector<int> result = solution(6);
+  EXPECT_THAT(result, ContainerEq(  //
+                          vector<int>{
+                              1,                     //
+                              2, 15,                 //
+                              3, 16, 14,             //
+                              4, 17, 21, 13,         //
+                              5, 18, 19, 20, 12,     //
+                              6, 7,  8,  9,  10, 11  //
+                          }  //
+                          )  //
+  );
+}
+
+TEST(Exam004_68645_Tests, Extra_TestCase_01) {
+  const vector<int> result = solution(2);
+  EXPECT_THAT(result, ContainerEq(  //
+                          vector<int>{
+                              1,     //
+                              2, 3,  //
+                          }  //
+                          )  //
+  );
+}
