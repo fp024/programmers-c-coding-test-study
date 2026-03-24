b15bf41945f491f4db2a8548736a7b3b1877c680
fp024 <aopenasus@naver.com>
2026-03-24 16:14:15 +0900

    MSVC_TOOLS_ROOT 경로 정리

--- toolchains/default_paths.cmake
+++ toolchains/default_paths.cmake
@@ -4,6 +4,11 @@ set(MINGW_ROOT "C:/MinGW-w64" CACHE PATH "Path to MinGW-w64 root")
 set(NINJA_ROOT "C:/Ninja/latest" CACHE PATH "Path to Ninja root")
 
 # WINDOWS SDK, VC 경로
-set(MSVC_TOOLS_ROOT "C:/VS2022/Community/VC/Tools/MSVC/14.44.35207" CACHE PATH "Path to MSVC Tools Root")
+# MSVC_ROOT는 표준 경로로 사용하기 위해 정션링크를 걸어주었다. 
+# - BuildTools 환경인 경우
+#   C:/MSVC_ROOT ==> [{드라이브_문자}:\VisualStudio\2022\BuildTools\VC]
+# - VS2022 커뮤니티 환경인 경우
+#   ...
+set(MSVC_TOOLS_ROOT "C:/MSVC_ROOT/Tools/MSVC/14.44.35207" CACHE PATH "Path to MSVC Tools Root")
 set(WINSDK_LIB_ROOT "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.26100.0" CACHE PATH "Path to Windows SDK Lib Root")
 set(WINSDK_INCLUDE_ROOT "C:/Program Files (x86)/Windows Kits/10/Include/10.0.26100.0" CACHE PATH "Path to Windows SDK Include Root")
